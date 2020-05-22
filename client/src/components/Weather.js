0ef3191fa3369bff7ce3377141cf9883a2a2f518
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-05-22 16:19:31 -0400

    uploaded svg weather icons

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import { useInterval } from '../hooks/useInterval'
+import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import cloudy from '../assets/cloudy.svg'
+
+
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null)
@@ -95,17 +99,17 @@ exclude=minutely&appid=${API_KEY}`
             </Grid>
             <Grid item xs={3}>
               {
-              day.weather[0].main === 'Rain' ||
-              day.weather[0].main === 'Thunderstorm' ||
-              day.weather[0].main === 'Snow' ? (
-                <>
-                  <strong>{day.weather[0].main}</strong>
-                  {/* <img 
-                    className="icon"
-                    src=""
-                    alt="icon" /> */}
-                </>
-              ) : day.weather[0].main
+                day.weather[0].main === 'Rain' ||
+                day.weather[0].main === 'Thunderstorm' ||
+                day.weather[0].main === 'Snow' ? (
+                  <>
+                    <strong>{day.weather[0].main}</strong>
+                    {/* <img 
+                      className="icon"
+                      src={cloudy}
+                      alt="icon" /> */}
+                  </>
+                ) : day.weather[0].main
               }
             </Grid>
           </Fragment>
@@ -114,17 +118,20 @@ exclude=minutely&appid=${API_KEY}`
   }
 
   return (
-    <Grid item xs={12}>
+    <Grid 
+      item 
+      xs={12}>
       <Paper>
         <h3>Weather</h3>
         {weatherData &&
           <p>
-            Currently: 
-            {Math.round(weatherData.current.temp)}&deg; <br />
-            Feels like: 
-            {Math.round(weatherData.current.feels_like)}&deg; <br />
-            Weather: 
+            Currently: {' '}
+            {Math.round(weatherData.current.temp)}&deg; ||
+            Feels like: {' '}
+            {Math.round(weatherData.current.feels_like)}&deg; ||
+            Weather: {' '}
             {weatherData.current.weather[0].main}
+            <hr />
           </p>
         }
         <Grid container>
